ebedcc7662ac04dbad3bbe1054e6f0aee2b5dbde
THEINDIANWIZARD <guttymanswaggg@hotmail.com>
2021-12-03 20:51:18 +0000

    This is commit 1 test

---
+++ C++/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  string hello = "Hello";
+  int values[] = {1, 2, 3, 4, 5, 0};
+  for (int i = 0; auto c = values[i]; i++)
+  {
+    cout << c << endl;
+  }
+  return 0;
+}
